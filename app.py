fa6b1ed524cd449175b8116d84f6e8fc9382e788
itsmesimhaa <hongirana.s@cloudjournee.com>
2025-04-04 11:29:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Welcome to Software_release_CICD pipeline, \n And this is an Comprehensive SOP on AWS CICD Pipelines'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
